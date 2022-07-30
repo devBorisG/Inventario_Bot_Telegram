cb9cade7e77e5c06824c971cc2cad529555c5ada
David Andrés Valencia García <david.andres.2801@gmail.com>
2022-07-29 22:11:32 -0500

    Optimizacion de funciones

--- Controller/inventarioController.py
+++ Controller/inventarioController.py
@@ -2,9 +2,6 @@
 def crearProducto(producto,precio,cantidad):
     pass
 
-def hola(nombre):
-    print("hola ", nombre)
-
 def calcEscases(cantidad,cantidadInicial):
     if(cantidad <= cantidadInicial*0.1):
         return True
